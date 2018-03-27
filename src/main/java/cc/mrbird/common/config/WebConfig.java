5e41fdeafdb7162fec24803ec1bd81a99b1347c5
mrbird <852252810@qq.com>
2018-03-27 18:00:51 +0800

    add XSSFilter🔒

---
+++ src/main/java/cc/mrbird/common/config/WebConfig.java
@@ -0,0 +1,32 @@
+package cc.mrbird.common.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import cc.mrbird.common.xss.XssFilter;
+
+@Configuration
+public class WebConfig {
+	
+	/**
+	 * XssFilter Bean
+	 */
+	@Bean
+	public FilterRegistrationBean xssFilterRegistrationBean() {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(new XssFilter());
+		filterRegistrationBean.setOrder(1);
+		filterRegistrationBean.setEnabled(true);
+		filterRegistrationBean.addUrlPatterns("/*");
+		Map<String, String> initParameters = new HashMap<String, String>();
+		initParameters.put("excludes", "/favicon.ico,/img/*,/js/*,/css/*");
+		initParameters.put("isIncludeRichText", "true");
+		filterRegistrationBean.setInitParameters(initParameters);
+		return filterRegistrationBean;
+	}
+	
+}
