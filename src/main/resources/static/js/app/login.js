69b4cbc9c222a2ffde8ee1319f40d8ca35520097
mrbird <852252810@qq.com>
2018-04-20 16:11:13 +0800

    优化登录体验⚡

--- src/main/resources/static/js/app/login.js
+++ src/main/resources/static/js/app/login.js
@@ -6,10 +6,10 @@ $(document).ready(function() {
         increaseArea: '20%'
     });
 
-    var panelOne = $('.form-panel.two').height(),
-        panelTwo = $('.form-panel.two')[0].scrollHeight;
+    var panelOne = $('.form-panel.two').height();
+    /*    panelTwo = $('.form-panel.two')[0].scrollHeight;
 
-    $('.form-panel.two').not('.form-panel.two.active').on('click', function(e) {
+   $('.form-panel.two').not('.form-panel.two.active').on('click', function(e) {
         e.preventDefault();
 
         $('.form-toggle').addClass('visible');
@@ -18,7 +18,7 @@ $(document).ready(function() {
         $('.form').animate({
             'height': panelTwo
         }, 200);
-    });
+    });*/
 
     $('.form-toggle').on('click', function(e) {
         e.preventDefault();
@@ -38,6 +38,7 @@ function reloadCode() {
 }
 
 function login() {
+	var $loginButton = $("#loginButton");
     var username = $(".one input[name='username']").val().trim();
     var password = $(".one input[name='password']").val().trim();
     var code = $(".one input[name='code']").val().trim();
@@ -54,6 +55,8 @@ function login() {
         $MB.n_warning("请输入验证码！");
         return;
     }
+    $loginButton.html("").append("<div class='login-loder'><div class='line-scale'><div></div><div></div><div></div><div></div><div></div></div></div>");
+    
     $.ajax({
         type: "post",
         url: ctx + "login",
@@ -68,14 +71,15 @@ function login() {
             if (r.code == 0) {
                 location.href = ctx + 'index';
             } else {
-            	if (r.msg == '验证码错误！') reloadCode();
+        		if (r.msg == '验证码错误！') reloadCode();
                 $MB.n_warning(r.msg);
+                $loginButton.html("登录");
             }
         }
     });
 }
 
-function regist() {
+/*function regist() {
     var username = $(".two input[name='username']").val().trim();
     var password = $(".two input[name='password']").val().trim();
     var cpassword = $(".two input[name='cpassword']").val().trim();
@@ -121,7 +125,7 @@ function regist() {
             }
         }
     });
-}
+}*/
 
 document.onkeyup = function(e) {
     if (window.event)
