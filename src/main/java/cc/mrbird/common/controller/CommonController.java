ab52c073968968b4c183be326d4d1c909871a4fc
mrbird <852252810@qq.com>
2018-06-22 09:49:44 +0800

    调整项目部署方式

--- src/main/java/cc/mrbird/common/controller/CommonController.java
+++ src/main/java/cc/mrbird/common/controller/CommonController.java
@@ -1,30 +1,26 @@
 package cc.mrbird.common.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.util.ResourceUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 public class CommonController {
 
 	@RequestMapping("common/download")
-	public void fileDownload(String fileName, Boolean delete, HttpServletResponse response,
-			HttpServletRequest request) {
+	public void fileDownload(String fileName, Boolean delete, HttpServletResponse response) {
 		String realFileName = System.currentTimeMillis() + fileName.substring(fileName.indexOf("_") + 1);
 		try {
 			response.setCharacterEncoding("utf-8");
 			response.setContentType("multipart/form-data");
 			response.setHeader("Content-Disposition",
 					"attachment;fileName=" + java.net.URLEncoder.encode(realFileName, "utf-8"));
-			String filePath = ResourceUtils.getURL("classpath:").getPath() + "static/file/" + fileName;
+			String filePath = "file/" + fileName;
 			File file = new File(filePath);
 			InputStream inputStream = new FileInputStream(file);
 			OutputStream os = response.getOutputStream();
--- src/main/java/cc/mrbird/common/util/FileUtils.java
+++ src/main/java/cc/mrbird/common/util/FileUtils.java
@@ -1,5 +1,8 @@
 package cc.mrbird.common.util;
 
+import cc.mrbird.common.domain.ResponseBo;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.List;
@@ -8,98 +11,98 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
-import org.springframework.util.ResourceUtils;
-
-import cc.mrbird.common.domain.ResponseBo;
-
 public class FileUtils {
 
-	/**
-	 * 文件名加UUID
-	 * 
-	 * @param filename
-	 * @return
-	 */
-	public static String makeFileName(String filename) {
-		return UUID.randomUUID().toString() + "_" + filename;
-	}
+    /**
+     * 文件名加UUID
+     *
+     * @param filename 文件名
+     * @return UUID_文件名
+     */
+    private static String makeFileName(String filename) {
+        return UUID.randomUUID().toString() + "_" + filename;
+    }
 
-	/**
-	 * 文件名特殊字符过滤
-	 * 
-	 * @param str
-	 * @return
-	 * @throws PatternSyntaxException
-	 */
-	public static String StringFilter(String str) throws PatternSyntaxException {
-		String regEx = "[`~!@#$%^&*+=|{}':; ',//[//]<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
-		Pattern p = Pattern.compile(regEx);
-		Matcher m = p.matcher(str);
-		return m.replaceAll("").trim();
-	}
+    /**
+     * 文件名特殊字符过滤
+     *
+     * @param fileName 文件名
+     * @return 过滤后的文件名
+     * @throws PatternSyntaxException 正则异常
+     */
+    public static String StringFilter(String fileName) throws PatternSyntaxException {
+        String regEx = "[`~!@#$%^&*+=|{}':; ',//[//]<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+        Pattern p = Pattern.compile(regEx);
+        Matcher m = p.matcher(fileName);
+        return m.replaceAll("").trim();
+    }
 
-	/**
-	 * 生成Excel文件
-	 * 
-	 * @param filename
-	 * @param list
-	 * @param clazz
-	 * @return
-	 */
-	public static ResponseBo createExcelByPOIKit(String filename, List<?> list, Class<?> clazz) {
+    /**
+     * 生成Excel文件
+     *
+     * @param filename 文件名称
+     * @param list     文件内容List
+     * @param clazz    List中的对象类型
+     * @return ResponseBo
+     */
+    public static ResponseBo createExcelByPOIKit(String filename, List<?> list, Class<?> clazz) {
 
-		if (list.isEmpty()) {
-			return ResponseBo.warn("导出数据为空！");
-		} else {
-			boolean operateSign = false;
-			String fileName = filename + ".xlsx";
-			fileName = makeFileName(fileName);
-			try {
-				String path = ResourceUtils.getURL("classpath:").getPath() + "static/file/" + fileName;
-				FileOutputStream fos = null;
-				fos = new FileOutputStream(path);
-				operateSign = ExcelUtils.$Builder(clazz)
-						// 设置每个sheet的最大记录数,默认为10000,可不设置
-						// .setMaxSheetRecords(10000)
-						.toExcel(list, "查询结果", fos);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
-			if (operateSign) {
-				return ResponseBo.ok(fileName);
-			} else {
-				return ResponseBo.error("导出Excel失败，请联系网站管理员！");
-			}
-		}
-	}
-	
-	/**
-	 * 生成Csv文件
-	 * @param filename
-	 * @param list
-	 * @param clazz
-	 * @return
-	 */
-	public static ResponseBo createCsv(String filename, List<?> list, Class<?> clazz) {
+        if (list.isEmpty()) {
+            return ResponseBo.warn("导出数据为空！");
+        } else {
+            boolean operateSign = false;
+            String fileName = filename + ".xlsx";
+            fileName = makeFileName(fileName);
+            try {
+                File fileDir = new File("file");
+                if (!fileDir.exists())
+                    fileDir.mkdir();
+                String path = "file/" + fileName;
+                FileOutputStream fos;
+                fos = new FileOutputStream(path);
+                operateSign = ExcelUtils.$Builder(clazz)
+                        // 设置每个sheet的最大记录数,默认为10000,可不设置
+                        // .setMaxSheetRecords(10000)
+                        .toExcel(list, "查询结果", fos);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            if (operateSign) {
+                return ResponseBo.ok(fileName);
+            } else {
+                return ResponseBo.error("导出Excel失败，请联系网站管理员！");
+            }
+        }
+    }
 
-		if (list.isEmpty()) {
-			return ResponseBo.warn("导出数据为空！");
-		} else {
-			boolean operateSign = false;
-			String fileName = filename + ".csv";
-			fileName = makeFileName(fileName);
-			try {
-				String path = ResourceUtils.getURL("classpath:").getPath() + "static/file/" + fileName;
-				operateSign = ExcelUtils.$Builder(clazz)
-						.toCsv(list, path);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
-			if (operateSign) {
-				return ResponseBo.ok(fileName);
-			} else {
-				return ResponseBo.error("导出Csv失败，请联系网站管理员！");
-			}
-		}
-	}
+    /**
+     * 生成Csv文件
+     *
+     * @param filename 文件名称
+     * @param list     文件内容List
+     * @param clazz    List中的对象类型
+     * @return ResponseBo
+     */
+    public static ResponseBo createCsv(String filename, List<?> list, Class<?> clazz) {
+
+        if (list.isEmpty()) {
+            return ResponseBo.warn("导出数据为空！");
+        } else {
+            boolean operateSign;
+            String fileName = filename + ".csv";
+            fileName = makeFileName(fileName);
+
+            File fileDir = new File("file");
+            if (!fileDir.exists())
+                fileDir.mkdir();
+            String path = "file/" + fileName;
+            operateSign = ExcelUtils.$Builder(clazz)
+                    .toCsv(list, path);
+            if (operateSign) {
+                return ResponseBo.ok(fileName);
+            } else {
+                return ResponseBo.error("导出Csv失败，请联系网站管理员！");
+            }
+        }
+    }
 }
