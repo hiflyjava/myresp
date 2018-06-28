beb57479f5fd9d6caf2d99333cb9202311fb1c6a
mrbird <852252810@qq.com>
2018-06-28 11:12:38 +0800

    优化代码

--- src/main/java/cc/mrbird/system/controller/LoginController.java
+++ src/main/java/cc/mrbird/system/controller/LoginController.java
@@ -19,76 +19,79 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController extends BaseController {
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@GetMapping("/login")
-	public String login() {
-		return "login";
-	}
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 
-	@PostMapping("/login")
-	@ResponseBody
-	public ResponseBo login(String username, String password, String code, Boolean rememberMe) {
-		if (!StringUtils.isNotBlank(code)) {
-			return ResponseBo.warn("验证码不能为空！");
-		}
-		Session session = super.getSession();
-		String sessionCode = (String) session.getAttribute("_code");
-		if (!code.toLowerCase().equals(sessionCode)) {
-			return ResponseBo.warn("验证码错误！");
-		}
-		password = MD5Utils.encrypt(username.toLowerCase(), password);
-		UsernamePasswordToken token = new UsernamePasswordToken(username, password, rememberMe);
-		try {
-			super.login(token);
-			this.userService.updateLoginTime(username);
-			return ResponseBo.ok();
-		} catch (UnknownAccountException | IncorrectCredentialsException | LockedAccountException e) {
-			return ResponseBo.error(e.getMessage());
-		} catch (AuthenticationException e) {
-			return ResponseBo.error("认证失败！");
-		}
-	}
+    @PostMapping("/login")
+    @ResponseBody
+    public ResponseBo login(String username, String password, String code, Boolean rememberMe) {
+        if (!StringUtils.isNotBlank(code)) {
+            return ResponseBo.warn("验证码不能为空！");
+        }
+        Session session = super.getSession();
+        String sessionCode = (String) session.getAttribute("_code");
+        session.removeAttribute("_code");
+        if (!code.toLowerCase().equals(sessionCode)) {
+            return ResponseBo.warn("验证码错误！");
+        }
+        password = MD5Utils.encrypt(username.toLowerCase(), password);
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password, rememberMe);
+        try {
+            super.login(token);
+            this.userService.updateLoginTime(username);
+            return ResponseBo.ok();
+        } catch (UnknownAccountException | IncorrectCredentialsException | LockedAccountException e) {
+            return ResponseBo.error(e.getMessage());
+        } catch (AuthenticationException e) {
+            return ResponseBo.error("认证失败！");
+        }
+    }
 
-	@GetMapping(value = "gifCode")
-	public void getGifCode(HttpServletResponse response) {
-		try {
-			response.setHeader("Pragma", "No-cache");
-			response.setHeader("Cache-Control", "no-cache");
-			response.setDateHeader("Expires", 0);
-			response.setContentType("image/gif");
+    @GetMapping(value = "gifCode")
+    public void getGifCode(HttpServletResponse response, HttpServletRequest request) {
+        try {
+            response.setHeader("Pragma", "No-cache");
+            response.setHeader("Cache-Control", "no-cache");
+            response.setDateHeader("Expires", 0);
+            response.setContentType("image/gif");
 
-			Captcha captcha = new GifCaptcha(146, 33, 4);
-			captcha.out(response.getOutputStream());
-			Session session = super.getSession();
-			session.removeAttribute("_code");
-			session.setAttribute("_code", captcha.text().toLowerCase());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            Captcha captcha = new GifCaptcha(146, 33, 4);
+            captcha.out(response.getOutputStream());
+            HttpSession session = request.getSession(true);
+            session.removeAttribute("_code");
+            session.setAttribute("_code", captcha.text().toLowerCase());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	@RequestMapping("/")
-	public String redirectIndex() {
-		return "redirect:/index";
-	}
+    @RequestMapping("/")
+    public String redirectIndex() {
+        return "redirect:/index";
+    }
 
-	@GetMapping("/403")
-	public String forbid() {
-		return "403";
-	}
+    @GetMapping("/403")
+    public String forbid() {
+        return "403";
+    }
 
-	@Log("访问系统")
-	@RequestMapping("/index")
-	public String index(Model model) {
-		User user = super.getCurrentUser();
-		model.addAttribute("user", user);
-		return "index";
-	}
+    @Log("访问系统")
+    @RequestMapping("/index")
+    public String index(Model model) {
+        User user = super.getCurrentUser();
+        model.addAttribute("user", user);
+        return "index";
+    }
 }
