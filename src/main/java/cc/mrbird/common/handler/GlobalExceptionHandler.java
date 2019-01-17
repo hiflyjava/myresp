92d7e0c1d0247562ede867fc58d6f0b1d94113e1
mgzu <2931170625@qq.com>
2019-01-17 19:06:37 +0800

    fix

--- src/main/java/cc/mrbird/common/handler/GlobalExceptionHandler.java
+++ src/main/java/cc/mrbird/common/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package cc.mrbird.common.handler;
 
 import cc.mrbird.common.domain.ResponseBo;
+import cc.mrbird.common.exception.FileDownloadException;
 import cc.mrbird.common.exception.LimitAccessException;
 import cc.mrbird.common.util.HttpUtils;
 
@@ -18,25 +19,30 @@ import javax.servlet.http.HttpServletRequest;
 @Order(value = Ordered.HIGHEST_PRECEDENCE)
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value = AuthorizationException.class)
-    public Object handleAuthorizationException(HttpServletRequest request) {
-        if (HttpUtils.isAjaxRequest(request)) {
-            return ResponseBo.error("暂无权限，请联系管理员！");
-        } else {
-            ModelAndView mav = new ModelAndView();
-            mav.setViewName("error/403");
-            return mav;
-        }
-    }
+	@ExceptionHandler(value = AuthorizationException.class)
+	public Object handleAuthorizationException(HttpServletRequest request) {
+		if (HttpUtils.isAjaxRequest(request)) {
+			return ResponseBo.error("暂无权限，请联系管理员！");
+		} else {
+			ModelAndView mav = new ModelAndView();
+			mav.setViewName("error/403");
+			return mav;
+		}
+	}
 
-    @ExceptionHandler(value = ExpiredSessionException.class)
-    public String handleExpiredSessionException() {
-        return "login";
-    }
+	@ExceptionHandler(value = ExpiredSessionException.class)
+	public String handleExpiredSessionException() {
+		return "login";
+	}
 
-    @ExceptionHandler(value = LimitAccessException.class)
-    public ResponseBo handleLimitAccessException(LimitAccessException e) {
-        return ResponseBo.error(e.getMessage());
-    }
+	@ExceptionHandler(value = LimitAccessException.class)
+	public ResponseBo handleLimitAccessException(LimitAccessException e) {
+		return ResponseBo.error(e.getMessage());
+	}
+
+	@ExceptionHandler(value = FileDownloadException.class)
+	public ResponseBo handleFileDownloadException(FileDownloadException e) {
+		return ResponseBo.error(e.getMessage());
+	}
 
 }
