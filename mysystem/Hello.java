8493b159480be69596a0ee51d112ad2ef4d1d904
harden <harden@qq.com>
2019-04-25 15:11:21 +0800

    修改 java bug hello 2

--- mysystem/Hello.java
+++ mysystem/Hello.java
@@ -1,10 +1,24 @@
-public class S{
+package main;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Hello {
+
+public static void main(String[] args) {
 	
-	public static void main (String[args]){
-		
-		System.out.prtilent("zzzzz");
-		
-	}
 	
-	
+	 Calendar calendar = Calendar.getInstance();
+     calendar.setTime(new Date());
+     calendar.set(Calendar.HOUR_OF_DAY, 23);
+     calendar.set(Calendar.MINUTE, 59);
+     calendar.set(Calendar.SECOND, 59);
+     Date zero = calendar.getTime();
+     long current = System.currentTimeMillis();
+     long zero1=current/(1000*3600*24)*(1000*3600*24)-TimeZone.getDefault().getRawOffset();
+     System.out.println(new Timestamp(zero1));
+     System.out.println(zero);
+}
 }
